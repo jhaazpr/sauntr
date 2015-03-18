3bb1fc00ecadbbaff29f8d72c5c8d2683afd7373
Jasper O'Leary <jhaazpr@gmail.com>
2015-03-17 23:52:11 -0700

    Complete functionality of reader, with functional changes
    Now, on the main loop, the reader behaves as does the writer by reading
    in values from the FSR analog input, and by recording a working number
    of pressure changes. The reader now differs from the writer in that,
    rather than making a post, it fetches the most recent step value from
    the server, and then actuates the actuation pin if there are more
    shoe steps than walker steps.
    
    Functions have been modularized to support actuating both the designated
    actuation pin (for the vibrators and ledStrip), as well as a new
    LED to indicate when the shoe is dealing with network overhead and not
    listening for sensor values.
    
    Note that if there are no entries within the last 10 min (default,
    but changeable in the ?lt part of the GET request), behavior value
    fetching is undefined.

--- reader/reader.ino
+++ reader/reader.ino
@@ -18,12 +18,12 @@
 // Use hardware SPI for the remaining pins
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
-                                         SPI_CLOCK_DIVIDER); // you can change this clock speed
+                                         SPI_CLOCK_DIV2); // you can change this clock speed
 
-#define WLAN_SSID       "EECS-PSK"           // cannot be longer than 32 characters!
-#define WLAN_PASS       "Thequickbrown"
+#define WLAN_SSID       "CalVisitor"           // cannot be longer than 32 characters!
+#define WLAN_PASS       ""
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
-#define WLAN_SECURITY   WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_UNSEC
 #define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) with no data 
                                    // received before closing the connection.  If you know the server
                                    // you're accessing is quick to respond, you can reduce this value.
@@ -37,18 +37,24 @@ Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ
 
 #define BUF_SIZE 128 //for reading
 #define WRITE_BUF_SIZE 16//for writing a float string
-#define SAMP_INTERVAL 1000 // milliseconds between running calculations and sending data
+#define SAMP_INTERVAL 200 // milliseconds between running calculations and sending data
+
+/* Pin initializations */
+const int buttonInputPin = 2;
+const int actuatorPin = 4;
+const int analogInPin0 = A0;
+const int connectNotifyPin = 7;
 
 /* Non-network-related initializations */
 float steps;
-const int buttonInputPin = 2;
-const int actuatorPin = 4;
 boolean newMeasure = true;
 int count = 0;
 float avg = 0;
+double walkerSteps = 0;
 int lastWeight = 0;
 time_t lastTime;
-const int thresh = 30;
+const int thresh = 80;
+const int pressThresh = 100;
 char readBuf[BUF_SIZE];
 char writeBuf[WRITE_BUF_SIZE];
 
@@ -65,26 +71,43 @@ void setup(void)
 {
   pinMode(buttonInputPin, INPUT);
   pinMode(actuatorPin, OUTPUT);
+  pinMode(analogInPin0, INPUT);
+  pinMode(connectNotifyPin, OUTPUT);
+  
+  digitalWrite(connectNotifyPin, HIGH);
   establishConnection();
-  digitalWrite(actuatorPin, HIGH);
-  delay(2000);
-  digitalWrite(actuatorPin, LOW);
+  digitalWrite(connectNotifyPin, LOW);
 }
 
 void loop(void)
 {
-  readJSONToBuffer();
-  Serial.println("In read buffer: ");
-  Serial.println(readBuf);
-  getFirstValue(readBuf, writeBuf);
-  double steps = atof(writeBuf);
-  Serial.print("Final value: ");
-  Serial.println(steps);
-  if (steps != 0.0) {
-    digitalWrite(actuatorPin, HIGH);
-    delay(2000);
-    digitalWrite(actuatorPin, LOW);
+  int sensorValue0 = analogRead(analogInPin0);
+  int newWeight = (sensorValue0);
+  Serial.println(newWeight);
+  if (newMeasure) {
+    lastTime = now();
+    newMeasure = false;
+  } else {
+    if ((now() - lastTime) < 10) {
+      if (newWeight > thresh && (lastWeight) < pressThresh) {
+        Serial.println("count");
+        count = count + 1;
+      }
+    } else {
+      lastTime = now();
+      newMeasure = true;
+      avg = 1.0 * (count);
+      Serial.print("Average: ");
+      Serial.println(avg);
+      walkerSteps = getServerSteps();
+      Serial.print("Walker: ");
+      Serial.println(walkerSteps);
+      compareAndActuate((double) avg, walkerSteps);
+      count = 0;
+    }
+    lastWeight = newWeight;
   }
+  
   delay(SAMP_INTERVAL);
 }
 
@@ -97,7 +120,7 @@ void loop(void)
 
 void establishConnection(void)
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   Serial.println(F("Hello, CC3000!\n")); 
 
   Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
@@ -148,8 +171,8 @@ void establishConnection(void)
 
 void handleFailedConnection(void)
 {
-	cc3000.disconnect();
-	establishConnection();
+  cc3000.disconnect();
+  establishConnection();
 }
 
 bool displayConnectionDetails(void)
@@ -173,11 +196,25 @@ bool displayConnectionDetails(void)
   }
 }
 
+/* Returns the most recent amount of steps from the writer */
+double getServerSteps(void)
+{
+  readJSONToBuffer();
+//  Serial.println("In read buffer: ");
+//  Serial.println(readBuf);
+  getFirstValue(readBuf, writeBuf);
+  double steps = atof(writeBuf);
+//  Serial.print("Final value: ");
+//  Serial.println(steps);
+  return steps;
+}
+
 /* Locs the first digit into readBuf */
 void readJSONToBuffer()
 {
   /* Set up socket and send input GET request */
   Serial.println("Connecting to website...");
+  digitalWrite(connectNotifyPin, HIGH); // notify user of pending connection
   Adafruit_CC3000_Client www = cc3000.connectTCP(ip, 80);
   if (www.connected()) {
     www.fastrprint(F("GET "));
@@ -188,7 +225,8 @@ void readJSONToBuffer()
     www.fastrprint(F("\r\n"));
     www.println();
   } else {
-    Serial.println(F("Connection failed")); 
+    Serial.println(F("Connection failed"));
+    actuate(connectNotifyPin);
 //    handleFailedConnection();   
     return;
   }
@@ -218,6 +256,7 @@ void readJSONToBuffer()
     }
   }
   www.close();
+  digitalWrite(connectNotifyPin, LOW);
   Serial.println(F("-------------------------------------"));
 }
 
@@ -236,3 +275,23 @@ void getFirstValue(char *readBuf, char *writeBuf) {
   j++;
   *(writeBuf + j) = '\0';
 }
+
+void compareAndActuate (double shoeSteps, double walkerSteps)
+{
+  if (shoeSteps > walkerSteps)
+  {
+    actuate(actuatorPin);
+  }
+}
+
+void actuate(int pin)
+{
+  for (int i = 0; i < 5; i++)
+  {
+    digitalWrite(pin, HIGH);
+    delay(250);
+    digitalWrite(pin, LOW);
+    delay(250);
+  }
+}
+
